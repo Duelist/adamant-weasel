7f1b18ad45167407aa822a3cac1ddff54b88d6b8
Ian Benedict <ianbenedict@gmail.com>
2015-11-18 21:11:10 -0500

    Updated install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 tar -xvzf packaging/kafka/kafka_2.10-0.8.2.0.tgz -C packaging/kafka
-exec mkdir logs
-exec psql -f scripts/create_database.sql
+mkdir logs
+psql -f scripts/create_database.sql
